98b5897126a7bc99563c09a07733eb39f18e461e
Derin DeNio <derindenio@gmail.com>
2024-02-02 19:28:24 -0700

    Added 'rappers' object and finished setting up GET request for API path

--- rap-names-api/server.js
+++ rap-names-api/server.js
@@ -3,18 +3,36 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
-const savage = {
-    'age': 29,
-    'birthName': 'Shéyaa Bin Abraham-Joseph',
-    'birthLocation': 'London, England'
+const rappers = {
+    '21 Savage':{
+        'age': 29,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'Chance the Rapper':{
+        'age': 29,
+        'birthName': 'Chancelor Johnathan Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'Dylan':{
+        'age': 29,
+        'birthName': 'Dylan',
+        'birthLocation': 'Dylan'
+    },
 }
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api', (request, response)=>{
-    response.json(savage)
+app.get('/api/:rapperName', (request, response)=>{
+    const rappersName = request.params.rapperName
+    if(rappers[rappersName]){
+        response.json(rappers[rappersName])
+    }else{
+        response.json(rappers['Dylan'])
+    }
+    //response.json(rappers)
 })
 
 app.listen(PORT, ()=>{
